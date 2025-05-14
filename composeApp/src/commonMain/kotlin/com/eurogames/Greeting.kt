c9061b772a2de7ec814612b9ad720b76bc97f83f
[josego16] <[josemagomez53@gmail.com]>
2025-05-14 19:04:14 +0200

    Inicio de rama sin cambios

--- composeApp/src/commonMain/kotlin/com/eurogames/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.eurogames
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
---
+++ composeApp/src/commonMain/kotlin/com/eurogames/ui/core/NavigationWrapper.kt
@@ -0,0 +1,2 @@
+package com.eurogames.ui.core
+
